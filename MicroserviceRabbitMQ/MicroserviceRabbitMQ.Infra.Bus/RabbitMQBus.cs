f947e1b4adeab3b809b8ef47ae1af7c6987a6a54
Alan Rodrigues Alves Pereira <alanrodriguesdev@outlook.com>
2024-09-02 21:57:17 -0300

    feat(RabbitMQ): added publish method

---
+++ MicroserviceRabbitMQ/MicroserviceRabbitMQ.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using MicroserviceRabbitMQ.Domain.Core.Bus;
+using MicroserviceRabbitMQ.Domain.Core.Commands;
+using MicroserviceRabbitMQ.Domain.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace MicroserviceRabbitMQ.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventsTypes;
+        public RabbitMQBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventsTypes = new List<Type>();
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+        public void Publish<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+
+                channel.QueueDeclare(eventName, false, false, false, null);
+
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("",eventName,null,body);
+            }        
+        }
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
