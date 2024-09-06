589515bca0772bb2d9e24e82129c47f5afe68c62
Alan Rodrigues Alves Pereira <alanrodriguesdev@outlook.com>
2024-09-06 19:27:57 -0300

    feat(Infra): initial commit of Infrastructure dependency project

---
+++ MicroserviceRabbitMQ/microserviceRabbitMQ.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroserviceRabbitMQ.Domain.Core.Bus;
+using MicroserviceRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace microserviceRabbitMQ.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
